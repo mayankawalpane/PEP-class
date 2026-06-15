ecfb1cc27e8f714ac1c48779afc48d957848f529
mayankawalpane <mayankawalpane25@gmail.com>
2026-06-15 11:31:43 +0530

    first commit

---
+++ program1.cpp
@@ -0,0 +1,43 @@
+    
+    //print a triangle of stars pattern using single loop
+    // # include <iostream>
+    // using namespace std;
+    // int main(){
+    // //     int a;
+    // // for(int i=0;i<6;i++){
+    // //     cout<<"*"<<endl;
+    // // }
+    // for(int i =0;i<6;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<"*"
+    //         ;
+    //     }
+    // }
+    
+    // }
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+       
+        cout <<"%-**\n",5,i,"****";
+    }
+    return 0;
+}
+
+
+#
+
+// 11111
+// 22222
+// 33333
+// 44444
+// 55555
+
+
+
+
