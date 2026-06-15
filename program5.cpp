34ed71fc025a446ac855a47e58fc0eb8dbeea031
mayankawalpane <mayankawalpane25@gmail.com>
2026-06-15 11:33:43 +0530

    5

--- program5.cpp
+++ program5.cpp
@@ -1,4 +1,14 @@
-1 3 5 7 9
-3 5 7 9 11
-5 7 9 11 13
-7
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        int num = 2 * i - 1;
+        for (int j = 1; j <= 5; j++) {
+            cout << num << " ";
+            num += 2;
+        }
+        cout << endl;
+    }
+    return 0;
+}
